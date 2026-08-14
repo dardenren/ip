f50ef9efd5aa155ffc8d73d7b247ff45cfc717d4
Darren <dattafuu@gmail.com>
2026-08-14 19:27:08 +0800

    Rename Duke to AmigoBot

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# AmigoBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/AmigoBot.java
@@ -0,0 +1,20 @@
+public class AmigoBot {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+
+        String banner = "    _          _           ____        _   \n"
+                + "   / \\   _ __ (_) __ _  ___| __ )  ___ | |_ \n"
+                + "  / _ \\ | '_ \\| |/ _` |/ _ \\  _ \\ / _ \\| __|\n"
+                + " / ___ \\| | | | | (_| | (_) | |_) | (_) | |_ \n"
+                + "/_/   \\_\\_| |_|_|\\__, |\\___/|____/ \\___/ \\__|\n"
+                + "                 |___/                        \n";
+
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hola amigo! I'm AmigoBot.");
+        System.out.println("What can I do for you, compadre?");
+        System.out.println(line);
+        System.out.println("Adios amigo! Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
