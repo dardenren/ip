d2d0a9e81ab3d4a12d755293dc29965ff3cd904c
Darren <dattafuu@gmail.com>
2026-08-15 20:54:06 +0800

    Add AmigoBotException custom exception class

---
+++ src/main/java/AmigoBotException.java
@@ -0,0 +1,9 @@
+/**
+ * Represents an exception specific to AmigoBot.
+ * Thrown when the user provides invalid input or an unrecognized command.
+ */
+public class AmigoBotException extends Exception {
+    public AmigoBotException(String message) {
+        super(message);
+    }
+}
