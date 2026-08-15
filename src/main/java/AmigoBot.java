51d21b17fef0f0b65b4285957db522620e804bb8
Darren <dattafuu@gmail.com>
2026-08-15 20:27:32 +0800

    Add the unmark feature

--- src/main/java/AmigoBot.java
+++ src/main/java/AmigoBot.java
@@ -52,6 +52,11 @@ public class AmigoBot {
                 String status = isDone[index] ? "X" : " ";
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("  [" + status + "] " + tasks[index]);
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                isDone[index] = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [ ] " + tasks[index]);
             } else {
                 tasks[taskCount] = input;
                 isDone[taskCount] = false;
